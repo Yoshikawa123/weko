801e4bf4b13629c192e80578d82bf555da4a9d3f
Yoshikawa123 <shoya.yoshikawa@ivis.co.jp>
2024-03-27 13:00:03 +0900

    コミットテスト1259

--- modules/weko-items-ui/weko_items_ui/bundles.py
+++ modules/weko-items-ui/weko_items_ui/bundles.py
@@ -5,7 +5,7 @@
 #
 # WEKO3 is free software; you can redistribute it
 # and/or modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; either version 2 of the
+# published by the Ffree Software Foundation; either version 2 of the
 # License, or (at your option) any later version.
 #
 # WEKO3 is distributed in the hope that it will be
