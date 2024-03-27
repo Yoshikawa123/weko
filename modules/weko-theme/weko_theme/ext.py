b37401c7ea6ab131b353ba3fe4b861b498319bc8
Yoshikawa123 <shoya.yoshikawa@ivis.co.jp>
2024-03-27 11:37:53 +0900

    テストコミット

--- modules/weko-theme/weko_theme/ext.py
+++ modules/weko-theme/weko_theme/ext.py
@@ -2,7 +2,7 @@
 #
 # This file is part of WEKO3.
 # Copyright (C) 2017 National Institute of Informatics.
-#
+#a
 # WEKO3 is free software; you can redistribute it
 # and/or modify it under the terms of the GNU General Public License as
 # published by the Free Software Foundation; either version 2 of the
